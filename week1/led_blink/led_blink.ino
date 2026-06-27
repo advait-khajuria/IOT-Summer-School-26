0e8950cc3a291bb74989085501a2da56668c8040
advait-khajuria <advaitkhajuria333@gmail.com>
2026-06-27 17:05:37 +0530

    feat: add serial blink counter
    Reduce delay time and add blink count logging.

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,10 +1,17 @@
+int blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(13, LOW);
-  delay(1000);
+  delay(500);
+
+  blinkCount++;
+  Serial.print("Blink Count: ");
+  Serial.println(blinkCount);
 }
